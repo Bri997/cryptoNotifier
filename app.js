55df1688ddd17eb68d5213bf7c7816bf023f3883
Bri997 <brian.musial@gmail.com>
2018-03-17 16:19:54 -0700

    added cards

--- app.js
+++ app.js
@@ -1,36 +1,47 @@
-const URL ="https://api.coinmarketcap.com/v1/ticker/" // top 100 coins on coinMarketCap
+const COINURL ="https://api.coinmarketcap.com/v1/ticker/?limit=10" // top 10 coins on coinMarketCap
+const MARKETCAPURL =" https://api.coinmarketcap.com/v1/global/" // gobal marketcap
 
-
-// function myFunction(){
-// console.log(query);
-//
-// }
 function getDataFromApi(callback){
-  console.log(callback);
+  // console.log(callback);
   const query = {
-    url: 'https://api.coinmarketcap.com/v1/ticker/?limit=10',
-    dataTyape: 'json',
+    url: COINURL,
+    dataType: 'json',
     type: 'Get',
     success: callback
-
   };
-
   $.ajax(query);
 }
 
+// function getMarketCapData(callback){
+//   const query ={
+//     url: MARKETCAPURL,
+//     dataType: 'json',
+//     type: 'Get',
+//     success: callback
+//   };
+//   $.ajax(query)
+// }
+
 getDataFromApi(displayCryptoData);
+// getMarketCapData(dipslayMarketCapData);
 
 function displayCryptoData(data){
-// var myCrpto = JSON.stringify(data)
-// for (let i =0 ; i < data.length; i++){
-// console.log(data[i].name);
-// var cryptoName = data[i].name
-// $("#data").append(`<div>${cryptoName}</div><p> Hi there </p>`);
-// }
-data.forEach(function(element){
-  console.log(element.name);
-});
+
+  for (let i = 0; i < data.length; i++){
+
+    var cryptoName = data[i].name;
+    var cryptoPriceUSD = data[i].price_usd
+    var cryptoPercentChange1hr = data[i].percent_change_1h
+    var cryptoPercentChange24hr = data[i].percent_change_24h
+
+      $(".data").append(`
+        <div class ="cryptoCards">
+          <h2 class ="cryptoName">${cryptoName}</h2> Current price is ${cryptoPriceUSD}<br>
+          1 hour perecent change is ${cryptoPercentChange1hr}<br>
+          24 hour change is ${cryptoPercentChange24hr}% <br></div>`
+
+        );
+
+      }
 
 }
-
-displayCryptoData();
